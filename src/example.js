afc2c941cf22ef0050105aab5585f6d958d46410
MarioQuiroga32 <marioqui135@gmail.com>
2020-05-08 16:22:00 +0200

    feat: created first test

--- src/example.js
+++ src/example.js
@@ -1,14 +1,3 @@
-
-export default class Example {
-  constructor() {
-    this.exampleVariable = 'example';
-  }
-
-  getExampleVariable() {
-    return this.exampleVariable;
-  }
-
-  setExampleVariable(exampleVariable) {
-    this.exampleVariable = exampleVariable;
-  }
-}
+export const Normalize = (str, { lang }) => {
+  return str.split(' ');
+};
---
+++ test/normalize.test.js
@@ -0,0 +1,30 @@
+import { Normalize } from "../src/example";
+
+// Cocinero -> COCINERO
+// Normalizar frases:
+//     Buscar:
+//         Lang code: “ES”
+//   “Cocinero,” -> [“COCINERO”]
+// “profesor inglés” -> [“PROFESOR”, “INGLES”]
+// “profesor de inglés” -> [“PROFESOR”, “INGLES”]   - stop word
+//   “COCINEROS” -> [“COCINERO”]
+// “PROFESORES” -> [“PROFESORE”]
+// “áäà” -> [“AAA”]
+// Lang code: “AR”
+//    “plural” : quitar s del principio
+// preposiciones: son otras.
+//     Mayusculas, minusculas.
+// Acentos.
+// Plurales.
+
+describe('Example Class Definition', function () {
+  test('Should return an array', function () {
+    const NormalizedStr = Normalize('Cocinero', { lang: 'ES' });
+    expect(Array.isArray(NormalizedStr)).toBeTruthy();
+  });
+
+  test('Should transform a string to upper case', function () {
+    const NormalizedStr = Normalize('Cocinero', { lang: 'ES' });
+    expect(NormalizedStr).toEqual(['COCINERO']);
+  });
+});
