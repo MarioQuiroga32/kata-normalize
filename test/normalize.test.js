2b8b340566bf2496a8387001e7a3e9d5a60dfa56
MarioQuiroga32 <marioqui135@gmail.com>
2020-05-08 17:13:41 +0200

    chore: remove comment

--- test/normalize.test.js
+++ test/normalize.test.js
@@ -1,22 +1,5 @@
 import { Normalize } from "../src/example";
 
-// Cocinero -> COCINERO
-// Normalizar frases:
-//     Buscar:
-//         Lang code: “ES”
-//   “Cocinero,” -> [“COCINERO”]
-// “profesor inglés” -> [“PROFESOR”, “INGLES”]
-// “profesor de inglés” -> [“PROFESOR”, “INGLES”]   - stop word
-//   “COCINEROS” -> [“COCINERO”]
-// “PROFESORES” -> [“PROFESORE”]
-// “áäà” -> [“AAA”]
-// Lang code: “AR”
-//    “plural” : quitar s del principio
-// preposiciones: son otras.
-//     Mayusculas, minusculas.
-// Acentos.
-// Plurales.
-
 describe('Example Class Definition', function () {
   test('Should return an array', function () {
     const NormalizedStr = Normalize('Cocinero', { lang: 'ES' });
